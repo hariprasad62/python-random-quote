db7e3c21936c97bf301a705f24b737d464f05231
Hariprasad Maramraj <hariprasadmaramraj@Hariprasads-MacBook-Air.local>
2020-06-23 17:29:37 +0530

    Explore Python on your own

--- get-quote.py
+++ get-quote.py
@@ -5,7 +5,12 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  print(quotes[13])
+  print(quotes[1], quotes[3], quotes[5])
+
+  f1 = open("quotes1.txt",'w')
+  for i in quotes:
+    f1.write(i)
+  f1.close()
 
 if __name__== "__main__":
   primary()
